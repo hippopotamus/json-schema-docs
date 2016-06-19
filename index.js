17770c9b9c77313da0ce59b9e506e822f4f21797
Armen Vartan <vartan.armen@gmail.com>
2016-06-18 22:57:48 -0400

    typos

--- index.js
+++ index.js
@@ -8,9 +8,8 @@ module.exports = function (locals) {
     if (typeof locals.schemaUrl !== 'string') {
         throw "`schemaUrl` must be a string."
     }
-    console.log(locals)
     /* injecting js inline, so that it only requires serving 1 file */
-    var template = jade.compileFile('docs.jade')(locals).replace(' src="bundle.js">', '>'+fs.readFileSync('./bundle.js'))
+    var template = jade.compileFile(__dirname+'/docs.jade')(locals).replace(' src="bundle.js">', '>'+fs.readFileSync('./bundle.js'))
     template = template.slice(0, template.indexOf('//# sourceMappingURL'))+'</script></body></html>' // quick hack removing browserify sourcemaps
     return template
 }
